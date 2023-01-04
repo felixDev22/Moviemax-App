30ff672b6e4bbfc143b7e29b160ccb617ff4b83a
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-04 17:01:09 +0300

    Feat: Add movie API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './style.css';
 import createPopup from './modules/popup.js';
+import getMovies from './modules/getMovies.js';
 
 const popupBtn = document.querySelectorAll('.popupbtn');
 
 popupBtn.forEach((item) => {
   item.addEventListener('click', createPopup);
 });
+
+getMovies();
