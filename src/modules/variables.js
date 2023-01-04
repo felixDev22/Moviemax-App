e76e3f04f85635e20cab4361166fdc0778ad7538
felixDev22 <felixnzomo@gmail.com>
2023-01-04 11:09:59 +0300

    Add modules folder, update variables

---
+++ src/modules/variables.js
@@ -0,0 +1,6 @@
+const likes = document.getElementById('like-btn');
+const numbers = document.getElementById('like-num');
+const comments = document.getElementById('comments');
+const movie = document.getElementById('movie');
+
+export { likes, numbers, comments, movie };
