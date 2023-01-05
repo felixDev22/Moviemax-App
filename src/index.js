52a2074c90586c36c8be942f51af29d6e1239611
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-05 18:06:46 +0300

    Feat: Add popup globally withou index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import './style.css';
 import displayUI from './modules/displayUI.js';
-import createPopup from './modules/popup.js';
+// import createPopup from './modules/popup.js';
 
 displayUI();
 
-window.addEventListener('load', () => {
-  const commentBtn = document.querySelectorAll('.btnClass');
-  commentBtn.forEach((element, index) => {
-    element.addEventListener('click', () => {
-      createPopup(index);
-    });
-  });
-});
+// window.addEventListener('load', () => {
+//   const commentBtn = document.querySelectorAll('.btnClass');
+//   commentBtn.forEach((element, index) => {
+//     element.addEventListener('click', () => {
+//       createPopup(index);
+//     });
+//   });
+// });
