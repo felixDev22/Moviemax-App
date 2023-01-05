58baa67e42ffc5641aa71ba94b26b30a5531521a
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-05 19:33:09 +0300

    Feat: Add likes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import './style.css';
 import displayUI from './modules/displayUI.js';
 import createPopup from './modules/popup.js';
+import { postLikes } from './modules/postLikes.js';
 
 displayUI();
 
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains('btnClass')) {
     createPopup(e.target.id);
+  }
+  if (e.target.classList.contains('fa-heart')) {
+    postLikes(e.target.id);
   }
 });
